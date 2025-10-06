4c6be5980dd80eeb631ec4e86773afb08a538849
MADAkwasi <agyemanmda@gmail.com>
2025-10-06 09:38:51 +0000

    feat(routes): setup role-based routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,22 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    children: [],
+    path: 'admin',
+    loadChildren: () =>
+      import('./features/admin/admin.routes').then((DEFAULT) => DEFAULT.ADMIN_ROUTES),
+  },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./features/employee/employee.routes').then((DEFAULT) => DEFAULT.EMPLOYEE_ROUTES),
   },
   {
     path: 'login',
     loadComponent: () => import('./shared/pages/login/login').then((Default) => Default.Login),
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
 ];
