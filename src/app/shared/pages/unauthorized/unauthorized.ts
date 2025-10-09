b437f5b3a8d069cb6a04c10dc0e3a8ce9f89ad53
MADAkwasi <agyemanmda@gmail.com>
2025-10-09 17:48:01 +0000

    feat(unauthorized): create 401 error page

--- src/app/shared/pages/unauthorized/unauthorized.ts
+++ src/app/shared/pages/unauthorized/unauthorized.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Header } from "@shared/components/header/header";
 
 @Component({
   selector: 'app-unauthorized',
-  imports: [],
+  imports: [Header, RouterLink],
   templateUrl: './unauthorized.html',
 })
 export class Unauthorized {}
