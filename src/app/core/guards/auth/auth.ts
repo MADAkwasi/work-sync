ecd86e88a710323f83a31c7f42c7dd91d5cd363f
MADAkwasi <agyemanmda@gmail.com>
2025-10-10 21:26:29 +0000

    feat(auth-guard): send toast when triggered

--- src/app/core/guards/auth/auth.ts
+++ src/app/core/guards/auth/auth.ts
@@ -1,12 +1,15 @@
 import { inject, Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '@core/services/auth/auth';
+import { ToastService } from '@core/services/toast/toast';
+import { toastNotifications } from '@shared/constants/toast';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
   private readonly authService = inject(AuthService);
+  private readonly toast = inject(ToastService);
   private readonly router = inject(Router);
 
   canActivate(): boolean {
@@ -14,6 +17,8 @@ export class AuthGuard implements CanActivate {
       return true;
     }
 
+    const { operations, status, messages } = toastNotifications;
+    this.toast.show(operations.accessDenied, status.error, messages.cantView);
     this.router.navigate(['/login']);
     return false;
   }
